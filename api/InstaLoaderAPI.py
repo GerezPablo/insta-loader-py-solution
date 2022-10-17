80974f2f6c407a2af2ecac19580f79815ee3b61b
P4BL1SK1 <pablogerez2000@gmail.com>
2022-10-16 23:44:38 -0300

    instaloader initial values fixed

--- api/InstaLoaderAPI.py
+++ api/InstaLoaderAPI.py
@@ -3,14 +3,14 @@ from utils.string import handle_url
 
 
 class InstaLoaderAPI:
-    def __init__(self, username = "instagram", password  = None):
+    def __init__(self, username=None, password=None):
         instaloader = Instaloader()
         instaloader.save_metadata = False
         instaloader.download_video_thumbnails = False
         instaloader.post_metadata_txt_pattern = ''
 
         self.instaloader = instaloader
-        self.username = username
+        self.username = username if username != None else "instagram"
 
         if (username and password):
             self.instaloader.login(self.username, password)
